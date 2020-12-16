872030b5a8195c5d4809a020fe001b0ca4db690a
vmnguyen <manhnguyen510@gmail.com>
2020-12-16 12:35:40 +0700

    add new tabs under Smart Scan

--- src/BurpBountyGui.java
+++ src/BurpBountyGui.java
@@ -60,6 +60,7 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
+
 public class BurpBountyGui extends javax.swing.JPanel {
 
     private IBurpExtenderCallbacks callbacks;
@@ -2118,7 +2119,14 @@ public class BurpBountyGui extends javax.swing.JPanel {
             }
         }
     }
-
+    protected javax.swing.JComponent makeTextPanel(String text) {
+        javax.swing.JPanel panel = new javax.swing.JPanel(false);
+        javax.swing.JLabel filler = new javax.swing.JLabel(text);
+        filler.setHorizontalAlignment(javax.swing.JLabel.CENTER);
+        panel.setLayout(new java.awt.GridLayout(1, 1));
+        panel.add(filler);
+        return panel;
+    }
     public void deleteProfile(JTable table) {
 
         Gson gson = new Gson();
@@ -2284,6 +2292,7 @@ public class BurpBountyGui extends javax.swing.JPanel {
         jPanel0 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         jPanel6 = new javax.swing.JPanel();
+        jPanel9 = new javax.swing.JPanel(); // init jPanel 9
         jLabel43 = new javax.swing.JLabel();
         jLabel44 = new javax.swing.JLabel();
         jLabel45 = new javax.swing.JLabel();
@@ -2394,7 +2403,7 @@ public class BurpBountyGui extends javax.swing.JPanel {
         jPanel1.setAutoscrolls(true);
 
         jPanel6.setEnabled(false);
-        //jPanel9.setEnabled(false);
+        jPanel9.setEnabled(false);
 
         jLabel43.setFont(new java.awt.Font("Lucida Grande", 1, 14)); // NOI18N
         jLabel43.setForeground(new java.awt.Color(255, 102, 51));
@@ -2515,7 +2524,11 @@ public class BurpBountyGui extends javax.swing.JPanel {
                 .addComponent(jScrollPane5, javax.swing.GroupLayout.DEFAULT_SIZE, 614, Short.MAX_VALUE))
         );
 
-        jtabpane0.addTab("     Active Profiles     ", jPanel10); //add new tab
+
+        javax.swing.JTabbedPane tabbedPane = new javax.swing.JTabbedPane();
+        javax.swing.JComponent panel1 = makeTextPanel("Panel #1");
+
+        jtabpane0.addTab("     Active Profiles     ", panel1); //add new tab
         jtabpane.addTab("     Active Profiles     ", jPanel3);
 
         table1.setAutoCreateRowSorter(true);
@@ -2693,7 +2706,7 @@ public class BurpBountyGui extends javax.swing.JPanel {
                 .addComponent(jtabpane, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                 .addContainerGap())
         );
-        /*
+
         javax.swing.GroupLayout jPanel9Layout = new javax.swing.GroupLayout(jPanel9);
         jPanel9.setLayout(jPanel9Layout);
         jPanel9Layout.setHorizontalGroup(
@@ -2704,44 +2717,46 @@ public class BurpBountyGui extends javax.swing.JPanel {
                     .addComponent(jtabpane0, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addGroup(jPanel9Layout.createSequentialGroup()
                         .addGroup(jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel43)
-                            .addComponent(jLabel44, javax.swing.GroupLayout.PREFERRED_SIZE, 575, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            //.addComponent(jLabel43)
+                            //.addComponent(jLabel44, javax.swing.GroupLayout.PREFERRED_SIZE, 575, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            )
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
             .addGroup(jPanel9Layout.createSequentialGroup()
                 .addGap(380, 380, 380)
-                .addComponent(jLabel45)
+                //.addComponent(jLabel45)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(newTagCombo2, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE)
+                //.addComponent(newTagCombo2, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel9Layout.setVerticalGroup(
             jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel9Layout.createSequentialGroup()
                 .addGap(10, 10, 10)
-                .addComponent(jLabel43)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel44)
+                //.addComponent(jLabel43)
+                //.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                //.addComponent(jLabel44)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(newTagCombo2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel45))
+                    //.addComponent(newTagCombo2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    //.addComponent(jLabel45)
+                    )
                 .addGap(18, 18, 18)
                 .addComponent(jtabpane0, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                 .addContainerGap())
         );
-        */
+
         javax.swing.GroupLayout jPanel0Layout = new javax.swing.GroupLayout(jPanel0);
         jPanel0.setLayout(jPanel0Layout);
         jPanel0Layout.setHorizontalGroup(
             jPanel0Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel0Layout.createSequentialGroup()
-                .addComponent(jPanel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel0Layout.setVerticalGroup(
             jPanel0Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel0Layout.createSequentialGroup()
-                .addComponent(jPanel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
